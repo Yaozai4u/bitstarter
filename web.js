d419b1146f2414eb0b62316150c81a3944f51f5c
Yaozai4u <yaozai4u@gmail.com>
2013-07-04 07:11:47 +0000

    bug fixed for web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var fs = require('fs');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
@@ -8,10 +8,12 @@ app.get('/', function(request, response) {
 
     var filename = "index.html";
     var r =  fs.readFileSync("index.html");
-    
-  response.send(r.toString('utf-8'));
+
+    response.send(r.toString('utf-8'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+
   console.log("Listening on " + port);
 });
